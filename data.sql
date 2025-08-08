0b0f8597fcd000070a0910c7ed57dd6ecbca1a37
Kushkumar18 <159297308+Kushkumar18@users.noreply.github.com>
2025-08-08 20:18:06 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,71 @@
+-- Insert Teams
+INSERT INTO team VALUES (1,'Red Bull Racing',2005,'Christian Horner',123);
+INSERT INTO team VALUES (2,'Aston Martin',2021,'Mike Krack',65);
+INSERT INTO team VALUES (3,'Mercedes',2010,'Toto Wolff',56);
+INSERT INTO team VALUES (4,'Scuderia Ferrari',1950,'Fred Vasseur',26);
+INSERT INTO team VALUES (5,'Alpine',2020,'Otmar Szafnauer',8);
+INSERT INTO team VALUES (6,'Alpha Tauri',2020,'Franz Tost',1);
+INSERT INTO team VALUES (7,'Alfa Romeo',1950,'Alessandro Alunni Bravi',6);
+INSERT INTO team VALUES (8,'Haas',2016,'Guenther Steiner',7);
+INSERT INTO team VALUES (9,'Williams',1977,'James Vowles',1);
+INSERT INTO team VALUES (10,'Mclaren',1966,'Andrea Stella',12);
+
+-- Insert Drivers
+INSERT INTO driver VALUES (1,1,'Max Verstappen',69,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (1,11,'Sergio Perez',54,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (2,14,'Fernando Alonso',45,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (3,44,'Lewis Hamilton',38,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (4,55,'Carlos Sainz',20,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (2,18,'Lance Stroll',20,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (3,63,'George Russell',18,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (10,4,'Lando Norris',8,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (8,27,'Nico Hulkenberg',6,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (4,16,'Charles Leclerc',6,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (7,77,'Valtteri Bottas',4,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (5,31,'Esteban Ocon',4,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (10,81,'Oscar Piastri',4,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (5,10,'Pierre Gasly',4,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (7,24,'Zhou Guanyu',2,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (6,22,'Yuki Tsunoda',1,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (8,20,'Kevin Magnussen',1,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (9,23,'Alex Albon',1,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (9,2,'Logan Sargeant',0,'In car',3,3,3,3,2,2,8);
+INSERT INTO driver VALUES (6,41,'Nyck de Vries',0,'In car',3,3,3,3,2,2,8);
+
+-- Insert Engines
+INSERT INTO engine VALUES (1,1,'Red Bull Powertrains');
+INSERT INTO engine VALUES (2,2,'Mercedes');
+INSERT INTO engine VALUES (3,3,'Mercedes');
+INSERT INTO engine VALUES (4,4,'Ferrari');
+INSERT INTO engine VALUES (5,5,'Renault');
+INSERT INTO engine VALUES (6,6,'Honda');
+INSERT INTO engine VALUES (7,7,'Ferrari');
+INSERT INTO engine VALUES (8,8,'Ferrari');
+INSERT INTO engine VALUES (9,9,'Mercedes');
+INSERT INTO engine VALUES (10,10,'Mercedes');
+
+-- Insert Engineers
+INSERT INTO engineers VALUES (1,'Gianpiero Lambiase',1);
+INSERT INTO engineers VALUES (2,'Hugh Bird',11);
+INSERT INTO engineers VALUES (3,'Xavier Marcos Padros',16);
+INSERT INTO engineers VALUES (4,'Riccardo Adami',55);
+INSERT INTO engineers VALUES (5,'Peter Bonnington',44);
+INSERT INTO engineers VALUES (6,'Riccardo Musconi',63);
+INSERT INTO engineers VALUES (7,'Josh Peckett',31);
+INSERT INTO engineers VALUES (8,'Karel Loos',10);
+INSERT INTO engineers VALUES (9,'William Joseph',4);
+INSERT INTO engineers VALUES (10,'Tom Stallard',81);
+INSERT INTO engineers VALUES (11,'Alex Chan',77);
+INSERT INTO engineers VALUES (12,'Jorn Becker',24);
+INSERT INTO engineers VALUES (13,'Ben Michell',18);
+INSERT INTO engineers VALUES (14,'Chris Cronin',14);
+INSERT INTO engineers VALUES (15,'Mark Slade',20);
+INSERT INTO engineers VALUES (16,'Gary Gannon',27);
+INSERT INTO engineers VALUES (17,'Mattia Spini',22);
+INSERT INTO engineers VALUES (18,'Pierre Hamelin',41);
+INSERT INTO engineers VALUES (19,'James Urwin',23);
+INSERT INTO engineers VALUES (20,'Gaetan Jego',2);
+
+-- Insert Races
+INSERT INTO race VALUES ('Bahrain',1);
+INSERT INTO race VALUES ('Saudi A
